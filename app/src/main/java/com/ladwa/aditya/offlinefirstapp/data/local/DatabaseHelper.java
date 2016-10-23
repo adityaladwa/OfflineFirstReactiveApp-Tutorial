1e5e3e2050b4b09c301d444fbb205ed49a75a6f3
Aditya <ladwa.aditya@gmail.com>
2016-10-23 14:22:30 +0530

    Fect: Created Database Helper class

--- app/src/main/java/com/ladwa/aditya/offlinefirstapp/data/local/DatabaseHelper.java
+++ app/src/main/java/com/ladwa/aditya/offlinefirstapp/data/local/DatabaseHelper.java
@@ -1,8 +1,31 @@
 package com.ladwa.aditya.offlinefirstapp.data.local;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Aditya on 23-Oct-16.
  */
 
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "OfflineFirstApp.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(DatabaseContract.Post.getPostCreateQuery());
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(DatabaseContract.Post.getUserDeleteQuery());
+        onCreate(sqLiteDatabase);
+    }
 }
