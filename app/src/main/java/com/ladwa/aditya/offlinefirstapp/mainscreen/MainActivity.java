8a6e0a95e97a3920ff047e0ecc23387b1bc9a432
Aditya <ladwa.aditya@gmail.com>
2016-10-23 14:08:24 +0530

    Fect: Implemented MainActivity Contract

--- app/src/main/java/com/ladwa/aditya/offlinefirstapp/mainscreen/MainActivity.java
+++ app/src/main/java/com/ladwa/aditya/offlinefirstapp/mainscreen/MainActivity.java
@@ -1,15 +1,53 @@
 package com.ladwa.aditya.offlinefirstapp.mainscreen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ladwa.aditya.offlinefirstapp.R;
+import com.ladwa.aditya.offlinefirstapp.data.local.models.Post;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainScreenContract.View {
+
+    private MainScreenContract.Presenter mPresenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void showPosts(List<Post> posts) {
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+
+    @Override
+    public void showComplete() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPresenter.subscribe();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPresenter.unsubscribe();
+    }
+
+    @Override
+    public void setPresenter(MainScreenContract.Presenter presenter) {
+        mPresenter = presenter;
     }
 }
--- app/src/main/java/com/ladwa/aditya/offlinefirstapp/mainscreen/MainScreenPresenter.java
+++ app/src/main/java/com/ladwa/aditya/offlinefirstapp/mainscreen/MainScreenPresenter.java
@@ -1,9 +1,24 @@
 package com.ladwa.aditya.offlinefirstapp.mainscreen;
 
 /**
- *
  * Created by Aditya on 23-Oct-16.
  */
 
-public class MainScreenPresenter {
+public class MainScreenPresenter implements MainScreenContract.Presenter {
+
+
+    @Override
+    public void loadPost() {
+
+    }
+
+    @Override
+    public void subscribe() {
+        loadPost();
+    }
+
+    @Override
+    public void unsubscribe() {
+        //Unsubscribe Rx subscription
+    }
 }
