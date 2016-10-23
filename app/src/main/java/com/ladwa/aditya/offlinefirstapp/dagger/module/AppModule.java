8dc019e2d8cebf59320898de0c87195d5650444d
Aditya <ladwa.aditya@gmail.com>
2016-10-23 13:23:56 +0530

    Fect: Created dagger modules

--- app/src/main/java/com/ladwa/aditya/offlinefirstapp/dagger/module/AppModule.java
+++ app/src/main/java/com/ladwa/aditya/offlinefirstapp/dagger/module/AppModule.java
@@ -1,8 +1,27 @@
 package com.ladwa.aditya.offlinefirstapp.dagger.module;
 
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * Created by Aditya on 23-Oct-16.
  */
 
+@Module
 public class AppModule {
+    Application mApplication;
+
+    public AppModule(Application mApplication) {
+        this.mApplication = mApplication;
+    }
+
+    @Provides
+    @Singleton
+    Application provideApplication() {
+        return mApplication;
+    }
 }
--- app/src/main/java/com/ladwa/aditya/offlinefirstapp/dagger/module/DataModule.java
+++ app/src/main/java/com/ladwa/aditya/offlinefirstapp/dagger/module/DataModule.java
@@ -1,8 +1,72 @@
 package com.ladwa.aditya.offlinefirstapp.dagger.module;
 
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by Aditya on 23-Oct-16.
  */
 
 public class DataModule {
+    String mBaseUrl;
+
+    public DataModule(String mBaseUrl) {
+        this.mBaseUrl = mBaseUrl;
+    }
+
+    @Provides
+    @Singleton
+    SharedPreferences providesSharedPreferences(Application application) {
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
+
+    @Provides
+    @Singleton
+    Cache provideHttpCache(Application application) {
+        int cacheSize = 10 * 1024 * 1024;
+        Cache cache = new Cache(application.getCacheDir(), cacheSize);
+        return cache;
+    }
+
+    @Provides
+    @Singleton
+    Gson provideGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        return gsonBuilder.create();
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkhttpClient(Cache cache) {
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        client.cache(cache);
+        return client.build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(mBaseUrl)
+                .client(okHttpClient)
+                .build();
+        return retrofit;
+    }
 }
