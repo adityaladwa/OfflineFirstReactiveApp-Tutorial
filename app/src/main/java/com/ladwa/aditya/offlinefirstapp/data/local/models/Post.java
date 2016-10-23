7bd0a7002da360fb5a4e6e7b0f6791fbb2226f8e
Aditya <ladwa.aditya@gmail.com>
2016-10-23 14:54:21 +0530

    Fect: Added Annotation to  model class

--- app/src/main/java/com/ladwa/aditya/offlinefirstapp/data/local/models/Post.java
+++ app/src/main/java/com/ladwa/aditya/offlinefirstapp/data/local/models/Post.java
@@ -1,13 +1,34 @@
 package com.ladwa.aditya.offlinefirstapp.data.local.models;
 
+import com.ladwa.aditya.offlinefirstapp.data.local.DatabaseContract;
+import com.pushtorefresh.storio.contentresolver.annotations.StorIOContentResolverColumn;
+import com.pushtorefresh.storio.contentresolver.annotations.StorIOContentResolverType;
+import com.pushtorefresh.storio.sqlite.annotations.StorIOSQLiteColumn;
+import com.pushtorefresh.storio.sqlite.annotations.StorIOSQLiteType;
+
 /**
  * Created by Aditya on 23-Oct-16.
  */
 
+@StorIOSQLiteType(table = DatabaseContract.Post.TABLE_NAME)
+@StorIOContentResolverType(uri = DatabaseContract.Post.CONTENT_URI_STRING)
 public class Post {
-    private final Integer userId;
+
+
+    @StorIOSQLiteColumn(name = DatabaseContract.Post.COLUMN_ID, key = true)
+    @StorIOContentResolverColumn(name = DatabaseContract.Post.COLUMN_ID, key = true)
     private final Integer id;
+
+    @StorIOSQLiteColumn(name = DatabaseContract.Post.COLUMN_USER_ID)
+    @StorIOContentResolverColumn(name = DatabaseContract.Post.COLUMN_USER_ID)
+    private final Integer userId;
+
+    @StorIOSQLiteColumn(name = DatabaseContract.Post.COLUMN_TITLE)
+    @StorIOContentResolverColumn(name = DatabaseContract.Post.COLUMN_TITLE)
     private final String title;
+
+    @StorIOSQLiteColumn(name = DatabaseContract.Post.COLUMN_BODY)
+    @StorIOContentResolverColumn(name = DatabaseContract.Post.COLUMN_BODY)
     private final String body;
 
     public Post(Integer userId, Integer id, String title, String body) {
