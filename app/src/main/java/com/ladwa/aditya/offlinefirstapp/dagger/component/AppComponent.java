330984c8cb7d04c463d48d2171d5bbcd9d538063
Aditya <ladwa.aditya@gmail.com>
2016-10-23 13:34:53 +0530

    Fect: Added Dagger Component

--- app/src/main/java/com/ladwa/aditya/offlinefirstapp/dagger/component/AppComponent.java
+++ app/src/main/java/com/ladwa/aditya/offlinefirstapp/dagger/component/AppComponent.java
@@ -1,8 +1,19 @@
 package com.ladwa.aditya.offlinefirstapp.dagger.component;
 
+import com.ladwa.aditya.offlinefirstapp.dagger.module.AppModule;
+import com.ladwa.aditya.offlinefirstapp.dagger.module.DataModule;
+import com.ladwa.aditya.offlinefirstapp.mainscreen.MainActivity;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
 /**
  * Created by Aditya on 23-Oct-16.
  */
 
-public class AppComponent {
+@Singleton
+@Component(modules = {AppModule.class, DataModule.class})
+public interface AppComponent {
+    void inject(MainActivity activity);
 }
